193937acfd816f4d5108f2edc758f78526426a76
Anil Verma <anilcs240@gmail.com>
2023-01-17 16:58:54 +0530

    added api result from csv files

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
-app= Flask(__name__)
+import pandas as pd
 
+app= Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperatue =23
+    filename= "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperatue = df.loc[df["    DATE"]==date]["   TG"].squeeze() / 10
     return {"stattion": station,
             "date": date,
             "temperatue" : temperatue}
